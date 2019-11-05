5cbd2e7078cc3f7047a13c7bc03eea55a4c43c61
gracejansen227 <graceowensjansen@gmail.com>
2019-11-05 17:03:32 -0600

    Changed UI libraries to react native eliements

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Font, AppLoading } from 'expo';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import { getEstablishmentTypes } from '../src/api/routes/find-establishment-types';
 import { getAllWineBars } from './src/api/routes/find-bars';
 import { Navigation } from 'react-native-navigation';
-import { Card, Screen, Tile, Title, Subtitle, ListView, NavigationBar } from '@shoutem/ui';
+import { Card, ListItem, Button, Icon } from 'react-native-elements'
 // import { goToBarSearch } from './src/navigation'
 // import { registerScreens } from './src/screens';
 // import HomeScreen from '../cheers_ui/src/screens/home-screen';
@@ -15,9 +14,7 @@ export default class App extends Component {
 
   constructor(props){
     super(props);
-
-	this.renderRow = this.renderRow.bind(this);
-    this.state = { bars: [], fontsAreLoaded: false };
+    this.state = { bars: []};
   }
 
   async getResult() {
@@ -25,58 +22,51 @@ export default class App extends Component {
    const barsData = response.data.restaurants.map(record => {
        return record.restaurant;
      });
-     this.setState({ bars: barsData, fontsAreLoaded: true });
+     this.setState({ bars: barsData });
   }
   
-  componentWillMount = async() => {
-	await Font.loadAsync({
-      'Rubik-Black': require('./node_modules/@shoutem/ui/fonts/Rubik-Black.ttf'),
-      'Rubik-BlackItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-BlackItalic.ttf'),
-      'Rubik-Bold': require('./node_modules/@shoutem/ui/fonts/Rubik-Bold.ttf'),
-      'Rubik-BoldItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-BoldItalic.ttf'),
-      'Rubik-Italic': require('./node_modules/@shoutem/ui/fonts/Rubik-Italic.ttf'),
-      'Rubik-Light': require('./node_modules/@shoutem/ui/fonts/Rubik-Light.ttf'),
-      'Rubik-LightItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-LightItalic.ttf'),
-      'Rubik-Medium': require('./node_modules/@shoutem/ui/fonts/Rubik-Medium.ttf'),
-      'Rubik-MediumItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-MediumItalic.ttf'),
-      'Rubik-Regular': require('./node_modules/@shoutem/ui/fonts/Rubik-Regular.ttf'),
-      'rubicon-icon-font': require('./node_modules/@shoutem/ui/fonts/rubicon-icon-font.ttf'),
-    });
+  componentWillMount(){
     this.getResult();
   }
-
-	renderRow(bar){
-		if (!bar){
-			return null;
-		}
-		if (!this.state.fontsAreLoaded){
-			return <AppLoading />;
-		}
-		return (
-			<View>
-				<Tile>
-					<Title styleName="md-gutter-bottom">{bar.name}</Title>
-					<Subtitle styleName="sm-gutter-horizontal">{bar.location.address}</Subtitle>
-				</Tile>
-				<Divider styleName="line" />
-			</View>
-		)
-	}
-
-	render() {
-		const { bars } = this.state.bars;
-		return (
-			<Screen>
-			<NavigationBar
-				title="Bars"
-				styleName="inline"
-			/>
-			 <ListView
-        		data={bars}
-        		renderRow={this.renderRow}
-      		/>
-			</Screen>
-		);
-	}
-
+  render() {
+    console.log('THIS IS BARS YO',this.state.bars);
+    return (
+	<ScrollView>
+	<View>
+		<Card containerStyle={{padding: 0}} >
+			{
+			this.state.bars.map((bar) => {
+			return (
+				<View>
+				<ListItem
+				key={bar.id}
+				title={bar.name}
+				subtitle={bar.location.address}
+				/>
+				<Button
+				small
+				raised
+				rounded
+				>Hello</Button>
+				</View>
+			);
+			})
+			}
+		</Card>
+	</View>
+	</ScrollView>
+    );
+  }
 }
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
